5de1f6e1ae0425e3ffb847b139ae449dd2291b2e
yaasin <ayadi.yassine@etu.uae.ac.ma>
2026-03-25 23:14:16 +0100

    creation de students/urls et ajoute le dans le path de school/urls

--- school/school/urls.py
+++ school/school/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('faculty.urls'))
+    path('', include('faculty.urls')),
+    path('student/', include('student.urls')),
 ]
